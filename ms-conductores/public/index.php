5f670c0b6b1c789d09b2524d784a285facfc8748
Isabela Cardenas <icardenas@uniboyaca.edu.co>
2026-06-01 17:56:22 -0500

    Isabela:[Arreglando segunda base de datos]

--- ms-conductores/public/index.php
+++ ms-conductores/public/index.php
@@ -1,27 +1,15 @@
 <?php
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
-
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../app/Config/db.php';
 
+use Slim\Factory\AppFactory;
+
 $app = AppFactory::create();
-$app->options('/{routes:.+}', fn($req, $res) => $res);
-$app->add(function (Request $request, $handler) {
-    $origin = $request->getHeaderLine('Origin') ?: '*';
-    $response = $handler->handle($request);
-    $response = $response
-        ->withHeader('Access-Control-Allow-Origin', $origin)
-        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
-        ->withHeader('Access-Control-Allow-Credentials', 'true');
-
-    if ($request->getMethod() === 'OPTIONS') {
-        return $response->withStatus(200);
-    }
-
-    return $response;
+$app ->get('/', function($request, $response){
+    $data = [
+        'message' => 'Funciona correctamente'
+    ];
+    $response->getBody()->write(json_encode($data));
+    return $response->withHeader('Content-Type', 'application/json');
 });
-(require __DIR__ . '/../app/Routes/conductores.php')($app);
-$app->run();
+$app -> run();
