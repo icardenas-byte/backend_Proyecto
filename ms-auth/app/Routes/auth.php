8e94f5c8e0426f2399cb63c7e2b6e15c0515eddc
Isabela Cardenas <icardenas@uniboyaca.edu.co>
2026-06-01 17:48:47 -0500

    Isabela:[Arreglando primera base de datos]

--- ms-auth/app/Routes/auth.php
+++ ms-auth/app/Routes/auth.php
@@ -1,21 +1,2 @@
-<?php
 
-use Slim\Routing\RouteCollectorProxy;
-use App\Controllers\AuthController;
-use App\Middleware\AuthMiddleware;
-
-return function ($app) {
-    $app->group('/api/auth', function (RouteCollectorProxy $group) {
-        $group->post('/login', [AuthController::class, 'login']);
-        
-        // Ruta GET temporal para prueba - QUITAR DESPUÉS
-        $group->get('/login', function ($request, $response) {
-            $response->getBody()->write('Usa POST para login, no GET');
-            return $response;
-        });
-        
-        $group->post('/logout', [AuthController::class, 'logout'])->add(new AuthMiddleware());
-        $group->get('/validate', [AuthController::class, 'validate'])->add(new AuthMiddleware());
-    });
-};
 
